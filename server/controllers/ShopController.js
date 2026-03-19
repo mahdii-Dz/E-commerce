d4c1d5e0b6f6075987d6839bf80142e2a6ecdf54
Mahdi Hadjidj <hadjidjmahdi@gmail.com>
2026-03-19 01:51:48 +0100

    hosting the mysql db

--- server/controllers/ShopController.js
+++ server/controllers/ShopController.js
@@ -3,10 +3,14 @@ import dotenv from "dotenv";
 dotenv.config();
 const pool = mysql
   .createPool({
-    host: process.env.DB_HOST || "localhost",
-    user: "root",
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_DATABASE,
+    port: process.env.DB_PORT,
+    ssl: {
+      rejectUnauthorized: true,  // Required for TiDB Cloud
+    },
     waitForConnections: true,
     connectionLimit: 10,
     queueLimit: 0,
